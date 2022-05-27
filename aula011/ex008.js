9c2ffe5bdec3baca030362a5211169d0b7832127
GuiHolanda <guilherme.lopes.holanda@hotmail.com>
2022-05-27 12:10:18 -0300

    Condições01

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 120
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade máxima permitida. Multado!')
+}
+
+console.log('Dirija sempre usando o cinto de segurança!')
